97bf9bcedbd77ca34e8ca8c0cad9e74a3b830eea
guvecchio <luis17ads@gmail.com>
2020-08-28 23:35:54 +0000

    28/08 - Atividade da revisão

--- arrow-functions.js
+++ arrow-functions.js
@@ -36,9 +36,58 @@ let aleatorio = function(){
     return Math.floor(Math.random() * 1000)
 }
 
+console.log('............................................')
+
 console.log(aleatorio())
+
+console.log('............................................')
 
 // ARROW FUNCTION com 0 argumento
 let aleatorio2 = () => Math.floor(Math.random() * 1000)
 
+
+// Situação alternativa 2: Função com mais de um argumento
+
+// Função tradicional com 1+ argumentos
+// Os parenteses dos argumentos são obrigatórios quando o número de argumentos é maior > 1
+let imc = function(peso, altura){
+    return peso / (altura ** 2)
+}
+
+console.log(imc(87, 1.78))
+
+console.log('............................................')
+
+let imc2 = (peso, altura) => peso / (altura ** 2)
+
+console.log(imc2(87, 1.78))
+
+console.log('............................................')
+
+// CONCLUSÃO: os parâmetros só podem ser omitidos da lista de argumentos de uma ARROW FUNCTION quando há UM e 
+// apenas um argumento.
+
+// Situação alternativa 3: funções com mais de uma linha no corpo
+
+// Função tradicional
+let fatorial = function(n){
+    let res = 1
+    for(let i = n; i > 1; i--){
+        res *= i
+    }
+    return res
+}
+
+//Arrow Function com corpo maior que uma linha
+
+let fatorial2 = n => {
+    let res = 1
+    for(let i = n; i > 1; i--){
+        res *= i
+    }
+    return res
+}
+
+console.log(fatorial(5))
+console.log(fatorial2(5))
 
