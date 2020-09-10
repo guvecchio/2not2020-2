e9982960e8f4b47be8976f856ef3e8155f5b9056
Gianvecchio <gustavo.caixapostal@gmail.com>
2020-09-10 20:26:23 -0300

    10/09 - Busca Binária com Implementação Recursiva

--- busca-binaria3.js
+++ busca-binaria3.js
@@ -2,12 +2,16 @@ let comp = 0
 
 // Pré-requisito para a busca-binária: o conjunto de dados PRECISA estar ordenado pelo critério de busca
 
-// IMPLEMENTAÇÃO ITERATIVA
-function buscaBinaria(lista, valorBusca, fnComp) {
-    let inicio = 0
-    let fim = lista.length -1
+// IMPLEMENTAÇÃO RECURSIVA:
+// Uma função chama a si mesma, com pelo menos um de seus parâmetro com valor alterado
+// Toda a função recursiva precisa ter pelo menos uma condição de saída, isto é, uma possibilidade de término
+// que não envolve uma chamada a ela mesma.
+// Os parâmetros inicio e fim são OPCIONAIS. Caso a função seja chamada sem especificá-las
+// elas assumirão os valores indicados
+// Primeiro parâmetros OBRIGATÓRIOS, depois OPCIONAIS, podemos ter só um ou outro tipo.
+function buscaBinaria(lista, valorBusca, fnComp, inicio = 0, fim = lista.length - 1) { // deixando o usuário indicar o inicio e o fim
     
-    while(fim >= inicio) {
+    if(fim >= inicio) { // o while é substituído, pois passa a executar somente uma vez, e resetar com a função recursiva
         // Math.floor tira as casas decimais de um número, arredondando para baixo
         let meio = Math.floor((fim + inicio) / 2)
 
@@ -16,18 +20,21 @@ function buscaBinaria(lista, valorBusca, fnComp) {
         // Verificar se o valor na posição é o valor da busca
         if(res == 0) {
             comp++
-            return meio
+            return meio // condição de saída
         }
         else if(res < 0) {
             comp += 2
-            fim = meio - 1
+            //fim = meio - 1
+            return buscaBinaria(lista, valorBusca, fnComp, inicio, meio - 1)
         }
         else { // res > 0
             comp += 2
-            inicio = meio + 1
+            //inicio = meio + 1
+            return buscaBinaria(lista, valorBusca, fnComp, meio + 1, fim)
         }
     }
     return -1 // Valor não encontrado
+    // condição de saída
 }
 
 
@@ -40,6 +47,18 @@ function comparaNome(obj, valorBusca) {
     else return 1 // vlorBusca > obj.first_name
 }
 
+let nums = [4, 16, 22, 29, 35, 44, 52, 58, 66, 71, 80, 88, 94]
+
+console.log('xxxxxxxxxxx RECURSIVA xxxxxxxxxxxxxx')
+
+console.log(buscaBinaria(nums, 66, (elpos, busca) =>{
+    if(busca === elpos) return 0
+    else if (busca < elpos) return -1
+    else return 1
+})) // posso passar ou NÃO os parâmetros DE INICIO E FIM pois na função chamada estão como OPCIONAIS
+
+// Termina a execução sem passar pelas linhas abaixo
+//process.exit(0) // 0 = saindo ok, sem erros
 
 let listaNomes = require('./dados/lista-nomes')
 
