6ecc4fb6c9d1c1ec8bc8cf180546ae1677ebc673
Gianvecchio <gustavo.caixapostal@gmail.com>
2020-10-01 18:56:39 -0300

    01/10 - Selection e Merge Sort

--- bubble-sort2.js
+++ bubble-sort2.js
@@ -38,10 +38,13 @@ function BublleSort(vetor, fnComp) {
 }
 
 /*
+    Quando preciso visualizar quais as propriedades do meu objeto. Aqui eu 
+    seleciono apenas o primeiro objeto do vetor, candidatos[0].
+
 for(let prop in candidatos[0]){
     console.log(prop)
 }
-process.exit(0)
+process.exit(0) // usado para não executar daqui para baixo.
 */
 
 const candidatos = require('./dados/candidatos-2018')
@@ -52,17 +55,30 @@ BublleSort(candidatos, (a, b) => a.NM_CANDIDATO > b.NM_CANDIDATO)
 console.timeEnd('TESTE CANDIDATOS')
 
 
-// Fazedo um map() para produzir um novo vetor só com os nomes dos candidatos.
+console.log(candidatos)
+
+/*
+
+MELHOR DOCUMENTAÇÃO PARA JS MDN (Mozilla Developer Network)
+
+Fazedo um map() para produzir um novo vetor só com os nomes dos candidatos.
+
 const nomesCandidatos = candidatos.map(o => o.NM_CANDIDATO)
 console.log(nomesCandidatos)
 
+    Teste com o localeCompare()
+    Só oos primeiros 1000 candidatos
 
-/*
-console.time('TESTE CANDIDATO 1000')
+cont cadidatos1000 = candidatos.slice(0, 999)
+
+console.time('TESTE CANDIDATOS 1000')
 // localeCompare() faz corretamente a ordenação considerando os acentos, mas é muito mais lento do que
 // a comparação binária padrão.
-BublleSort(candidatos, (a, b) => a.NM_URNA_CANDIDATO.localeCompare(b.NM_URNA_CANDIDATO, 'pt-BR') < 0)
+BublleSort(candidatos1000, (a, b) => a.NM_URNA_CANDIDATO.localeCompare(b.NM_URNA_CANDIDATO, 'pt-BR') < 0)
 console.timeEnd('TESTE CANDIDATOS 1000')
 
 console.log(candidatos)
+
 */
+
+// PRINCIPAL PROBLEMA DO bubbleSort - Troca mal, por isso troca muito.
